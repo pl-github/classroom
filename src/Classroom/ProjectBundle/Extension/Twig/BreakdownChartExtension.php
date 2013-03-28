55adff483d27c1794a46026a1fecede6e19cd56f
Michael van Engelshoven <michael@van-engelshoven.de>
2013-03-28 17:58:33 +0100

    Added breakdown chart twig extension
    This extension will convert breakdown data from the entity into json
    encoded chart data, needed by the javascript.

---
+++ src/Classroom/ProjectBundle/Extension/Twig/BreakdownChartExtension.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace Classroom\ProjectBundle\Extension\Twig;
+
+use Classroom\ProjectBundle\Entity\Revision;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class BreakdownChartExtension extends \Twig_Extension
+{
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+    
+    public function getFilters()
+    {
+        return array(
+            'breakdownChart' => new \Twig_Filter_Method($this, 'breakdownChart', array('is_safe' => array('html'))),
+        );
+    }
+
+    public function breakdownChart(Revision $revision)
+    {
+        $data = array();
+        $project = $revision->getProject()->getKey();
+        
+        foreach ($revision->getBreakdown() as $grade => $count) {
+            if ($count > 0) {
+                $link = $this->urlGenerator->generate(
+                    'classroom_project_nodes',
+                    array('grade' => $grade, 'projectKey' => $project),
+                    UrlGeneratorInterface::ABSOLUTE_PATH
+                );
+                $data[] = array(
+                    'label' => strtoupper($grade) . "-Grade",
+                    'count' => $count,
+                    'link'  => $link,
+                );
+            }
+        }
+
+        return json_encode($data);
+    }
+
+    public function getName()
+    {
+        return 'breakdownchart_extension';
+    }
+}
